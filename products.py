faa98e5f6a148cad0bdd8a1eebf78bb5b1295532
Daisy <soniahsiao16@gmail..com>
2021-03-09 00:29:22 +0800

    add continue

--- products.py
+++ products.py
@@ -1,13 +1,14 @@
+# 讀取檔案
 products = []
 with open('products.csv', 'r', encoding='utf-8') as f:
     for line in f:
-        cell = line.strip().split(',')
-        name = cell[0]
-        price = cell[1]
-        print(cell)
-        # print(type(cell))  # split()切割完的結果是清單
+        if '商品種類, 價格' in line:
+            continue  # continue省略這一回後面的內容，跳到下一回繼續（不是跳出迴圈，break才是逃出回圈）通常放在loop裡面很高的位置，因為這樣才有東西可以跳過
+        name, price = line.strip().split(',')
+        products.append([name, price])
+        # print(type(line.strip().split(',')))  # split()切割完的結果是清單
 
-
+# 讓使用者輸入
 while True:
     name = input('請輸入商品名稱：')
     if name == 'q':  # quit
@@ -15,12 +16,13 @@ while True:
     price = input('請輸入商品價格：')
     # p = []  p.append(name)  p.append(price)     #p = [name, price] products.append(p)
     products.append([name, price])
-# print(products)
+print(products)
 
+# 印出所有購買紀錄
 for p in products:
     print(p[0], '的價格為', p[1])
 
-
+# 寫入檔案
 with open('products.csv', 'w', encoding='utf-8') as f:
     f.write('商品種類,價格\n')
     for p in products:

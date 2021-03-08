0898a8b0b9253a982ee464b65b204a483193ac9b
Daisy <soniahsiao16@gmail..com>
2021-03-09 00:41:17 +0800

    add check file exist

--- products.py
+++ products.py
@@ -1,12 +1,20 @@
-# 讀取檔案
+import os  # operating system 載入作業系統
+
 products = []
-with open('products.csv', 'r', encoding='utf-8') as f:
+
+if os.path.isfile('products.csv'):  # 檢查檔案在不在
+    print('Yeah! 找到檔案了!')
+    # 讀取檔案
+    with open('products.csv', 'r', encoding='utf-8') as f:
     for line in f:
         if '商品種類, 價格' in line:
             continue  # continue省略這一回後面的內容，跳到下一回繼續（不是跳出迴圈，break才是逃出回圈）通常放在loop裡面很高的位置，因為這樣才有東西可以跳過
         name, price = line.strip().split(',')
         products.append([name, price])
         # print(type(line.strip().split(',')))  # split()切割完的結果是清單
+else:
+    print('找不到檔案...')
+
 
 # 讓使用者輸入
 while True:
